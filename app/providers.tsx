e80cd5b6480a33a6b0871d9969192f52ab548d84
jpbtk <zakuro877@gmail.com>
2025-05-28 23:36:40 +0900

    Initial commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,11 @@
 
 import { useState } from 'react'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [supabaseClient] = useState(() =>
-    createBrowserSupabaseClient({
+    createPagesBrowserClient({
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,    // '/api/auth'
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     })
