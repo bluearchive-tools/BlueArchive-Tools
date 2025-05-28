0eb3cbe3bb5a7949cdacdb6569e6fc2979a124c5
jpbtk <zakuro877@gmail.com>
2025-05-28 22:04:56 +0900

    Initial commit

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,11 +1,11 @@
 // app/mypage/page.tsx
 import { cookies } from 'next/headers'
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { redirect } from 'next/navigation'
 
 export default async function MyPage() {
   // クッキー情報をもとにサーバー側でセッション取得
-  const supabase = createServerComponentSupabaseClient({ cookies })
+  const supabase = createServerComponentClient({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
 
   if (!session) {
