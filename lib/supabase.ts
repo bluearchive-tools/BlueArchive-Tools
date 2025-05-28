6292fa5c640af81455fc63d7cbe4be430697f858
jpbtk <zakuro877@gmail.com>
2025-05-28 23:07:49 +0900

    Initial commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 // lib/supabase.ts
-import { createClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,       // → '/api/auth'
