482c171a99f0fcd1bc4fbf3d82dbd1bf5f38326c
jpbtk <zakuro877@gmail.com>
2025-05-28 23:22:02 +0900

    Initial commit

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,13 +1,23 @@
 // app/api/health/route.ts
 import { NextResponse } from 'next/server';
-import { supabase } from '../../../lib/supabase';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 export async function GET() {
+  const supabase = createRouteHandlerClient(
+    { cookies },
+    {
+      supabaseUrl: process.env.SUPABASE_URL!,
+      supabaseKey:  process.env.SUPABASE_ANON_KEY!,
+    }
+  )
   // profiles テーブルから１件だけ取ってみる
   const { data, error } = await supabase
     .from('profiles')
     .select('id')
     .limit(1);
+
+  
 
   if (error) {
     return NextResponse.json(
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -5,7 +5,14 @@ import { redirect } from 'next/navigation'
 
 export default async function MyPage() {
   // クッキー情報をもとにサーバー側でセッション取得
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = createServerComponentClient(
+        { cookies },
+        {
+          // サーバーサイドでは絶対URL／キーを使う
+          supabaseUrl: process.env.SUPABASE_URL!,
+          supabaseKey:  process.env.SUPABASE_ANON_KEY!,
+        }
+      )
   const { data: { session } } = await supabase.auth.getSession()
 
   if (!session) {
