93ee7ac6853f2caee9300abcc1b293ce1d9ddfea
jpbtk <zakuro877@gmail.com>
2025-05-28 23:39:28 +0900

    Initial commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,10 +7,7 @@ import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [supabaseClient] = useState(() =>
-    createPagesBrowserClient({
-      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,    // '/api/auth'
-      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    })
+    createPagesBrowserClient()
   )
 
   return (
