aa02920409098ed58878ac42cba1da4f261803b6
bluearchive-tools <bluearchive.tools@gmail.com>
2025-05-30 11:07:58 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
         source: '/api/auth/:path*',
         destination: `${process.env.SUPABASE_URL}/auth/v1/:path*`
       },
-      // 他にもREST APIやストレージをプロキシしたい場合は追加
+      // 他にもREST APIやストレージをプロキシしたい場合は追加a
       // {
       //   source: '/api/rest/:path*',
       //   destination: `${process.env.SUPABASE_URL}/rest/v1/:path*`
